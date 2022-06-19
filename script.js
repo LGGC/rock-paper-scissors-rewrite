81f3cc8de57017bfce03b957c344a57633f586fc
Liran Goldman <liran12369@gmail.com>
2022-06-19 15:54:30 +0300

    add function to take care of user input and add a game function to run 5 rounds

--- script.js
+++ script.js
@@ -15,3 +15,69 @@ function computerPlay() {
             break;
     }
 }
+
+function capitaliseFirstLetter(str) {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let uiPlayerSelection = capitaliseFirstLetter(playerSelection);
+    if (playerSelection === 'rock' && computerSelection === 'Scissors'
+        || playerSelection === 'paper' && computerSelection === 'Rock'
+        || playerSelection === 'scissors' && computerSelection === 'Paper') {
+        // What if the player wins?
+        return `You Win! ${uiPlayerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === 'rock' && computerSelection === 'Paper'
+        || playerSelection === 'paper' && computerSelection === 'Scissors'
+        || playerSelection === 'scissors' && computerSelection === 'Rock') {
+        // What if the computer wins?
+        return `You Lose! ${computerSelection} beats ${uiPlayerSelection}`;
+    } else {
+        return `Draw! ${uiPlayerSelection} and ${computerSelection}`
+    }
+
+}
+
+function userInput() {
+    let userChoice = ' ';
+    while (
+        userChoice.toLowerCase() != 'rock' &&
+        userChoice.toLowerCase() != 'paper' &&
+        userChoice.toLowerCase() != 'scissors') {
+        userChoice = prompt("Enter Rock, Paper, Scissors!")
+    }
+
+    return userChoice;
+}
+
+function game() {
+    let pointsPC = 0;
+    let pointsPlayer = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log("Round " + i);
+        let currentRound = playRound(userInput(), computerPlay());
+        if (currentRound.toLowerCase().includes('draw')) {
+            console.log(currentRound);
+        } else if (currentRound.toLowerCase().includes('lose')) {
+            console.log(currentRound);
+            pointsPC++;
+        } else if (currentRound.toLowerCase().includes('win')) {
+            console.log(currentRound);
+            pointsPlayer++;
+        }
+
+        console.log(`You now have ${pointsPlayer} points!\nThe NPC has ${pointsPC} points`);
+    }
+
+    if (pointsPC > pointsPlayer) {
+        console.log("You lost!");
+    } else if (pointsPlayer > pointsPC) {
+        console.log("You won!");
+    } else {
+        console.log("Draw!");
+    }
+}
+
+game()
